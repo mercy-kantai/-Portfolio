bef90023e3d43b7111276f28b274e143a3001c7f
mercy-kantai <kantaiemilymercy@gmail.com>
2024-10-21 16:39:02 +0300

    add portfolio

--- portfolio/my-portfolio/src/app/page.tsx
+++ portfolio/my-portfolio/src/app/page.tsx
@@ -1,9 +1,9 @@
-import Header from "./Components/Navbar";
-import Hero from "./Components/Hero";
-import About from "./Components/navbar";
-import Services from "./Components/Services";
-import History from "./Components/History";
-import Footer from "./Components/Footer";
+import Header from "./Components/Navbar/navbar";
+import Hero from "./Components/Hero/hero";
+import About from "./Components/About";
+import Services from "./Components/Services/services";
+import History from "./Components/History/history";
+import Footer from "./Components/Footer/footer";
 
 export default function Home() {
   return (
